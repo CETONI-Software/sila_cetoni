af64f5f3142b2762c6055348943d0f1cefe452a7
Florian Meinicke <florian.meinicke@cetoni.de>
2020-10-23 13:11:08 +0200

    Add script and service to monitor WLAN activity
    Change-Id: I708e65a596ed8ba8ad122caee529c1eac15241d9

---
+++ monitor_wlan0.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Helper script to monitor WLAN activity (e.g. signal strength) and write the
+# results in a to be processed CSV file
+#
+#set -x
+
+FILENAME="$HOME/sila_qmix/log/wlan0-`date +%Y-%m-%d.%H:%M:%S`.csv"
+
+echo "time; rx bitrate; tx bitrate; signal" > $FILENAME
+
+while true; do
+    bit_rate=`iw dev wlan0 link | grep bitrate | awk -F: ' BEGIN { ORS=";" } { print $2 } '`
+    signal_strength=`iw dev wlan0 link | grep signal | awk -F: ' { print $2 } '`
+    time=`date +%Y-%m-%d.%H:%M:%S`
+    echo "$time;$bit_rate$signal_strength" >> $FILENAME
+
+    sleep 10
+done
