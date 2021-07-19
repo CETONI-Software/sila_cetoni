e5494d5cd9598f0079cb9b9f3f6060a7c98f76e1
Florian Meinicke <florian.meinicke@cetoni.de>
2021-07-19 08:47:18 +0200

    Refactoring
    Change-Id: Ie0d02bf3b1aff6fbe65bfde97da4efbce279d386

---
+++ application/singleton.py
@@ -0,0 +1,34 @@
+"""
+________________________________________________________________________
+
+:PROJECT: sila_cetoni
+
+*Singleton*
+
+:details: Singleton:
+    A singleton class to be used as metaclass of another class in order to turn
+    that class into a singleton
+
+:file:    singleton.py
+:authors: Florian Meinicke
+
+:date: (creation)          2021-07-19
+:date: (last modification) 2021-07-19
+
+________________________________________________________________________
+
+**Copyright**:
+  This file is provided "AS IS" with NO WARRANTY OF ANY KIND,
+  INCLUDING THE WARRANTIES OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+  For further Information see LICENSE file that comes with this distribution.
+________________________________________________________________________
+"""
+
+# taken from https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
