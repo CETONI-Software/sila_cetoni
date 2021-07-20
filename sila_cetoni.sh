68738885288338a63ddfaf3c2fa252a39932b499
Florian Meinicke <florian.meinicke@cetoni.de>
2021-07-20 09:07:02 +0200

    Add option to disable logging to file on Linux
    and use that option for the systemd service. We're going to rely on
    systemd's journal logs from now on instead of creating our own log
    files. This has the advantage that we don't need to care about
    rotation and deletion of old files as this will all be handled by
    systemd-journald.

--- sila_cetoni.sh
+++ sila_cetoni.sh
@@ -6,6 +6,11 @@
 #
 # Parameters to this script are passed right on to the sila_cetoni.py script
 #
+# By default this script creates a log file for each run in the sila_cetoni/log/
+# directory. To prevent this (e.g. because logging to the systemd-journal is 
+# sufficient) you can set the SILA_CETONI_NO_LOG_FILE environment variable to
+# any value (e.g. 1).
+#
 #set -x
 
 sleep 1 # prevent sporadically not working 2nd pump
@@ -13,7 +18,7 @@ sleep 1 # prevent sporadically not working 2nd pump
 # change this path to point to your CETONI SDK installation
 grep -qi raspbian /etc/os-release 2>/dev/null && {
     # default directory for RaspberryPi
-    export CETONI_SDK_PATH="$HOME/CETONI_SDK_Raspi"
+    export CETONI_SDK_PATH="$HOME/QmixSDK_Raspi"
 } || {
     # default directory for generic Linux
     export CETONI_SDK_PATH="$HOME/CETONI_SDK_Linux"
@@ -25,15 +30,17 @@ export LD_LIBRARY_PATH="$CETONI_SDK_PATH/lib":"$LD_LIBRARY_PATH"
 
 curr_dir=$(pwd)
 
-# use runtime dir if available which is mounted as tmpfs to save some writes to the SD card
-${RUNTIME_DIRECTORY:=$XDG_RUNTIME_DIR} 2>/dev/null # RUNTIME_DIRECTORY might come from systemd
-LOG_DIR="${RUNTIME_DIRECTORY:-$curr_dir}/log"
-LOG_FILE="$LOG_DIR/sila_cetoni-`date +%Y-%m-%d.%H:%M:%S`.log"
-mkdir -p $LOG_DIR
-# Redirect stdout into a named pipe
-exec > >( tee -i "$LOG_FILE" )
-# Also redirect stderr
-exec 2>&1
+if [ -z $SILA_CETONI_NO_LOG_FILE ]; then
+    # use runtime dir if available which is mounted as tmpfs to save some writes to the SD card
+    ${RUNTIME_DIRECTORY:=$XDG_RUNTIME_DIR} 2>/dev/null # RUNTIME_DIRECTORY might come from systemd
+    LOG_DIR="${RUNTIME_DIRECTORY:-$curr_dir}/log"
+    LOG_FILE="$LOG_DIR/sila_cetoni-`date +%Y-%m-%d.%H:%M:%S`.log"
+    mkdir -p $LOG_DIR
+    # Redirect stdout into a named pipe
+    exec > >( tee -i "$LOG_FILE" )
+    # Also redirect stderr
+    exec 2>&1
+fi
 
 python3 sila_cetoni.py $@
 cd $curr_dir
