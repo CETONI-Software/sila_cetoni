8cbd41d766aa6b79dd4df44ce0f1ffa2b6141587
Florian Meinicke <florian.meinicke@cetoni.de>
2021-11-23 07:46:17 +0100

    Fix install.cmd script
    Use `python -m virtualenv` instead of `virtualenv`

--- install.cmd
+++ install.cmd
@@ -15,12 +15,22 @@ if %pip% == "" (
     exit /b 1
 )
 
+set python=
+for /f %%i in ("python python3") do (
+    for /f "usebackq tokens=*" %%i in (`where python3`) do set python="%%i"
+)
+
+if %pip% == "" (
+    echo "Could not find a python executable! Either install python 3 or adjust your PATH environment variable to point to the directory where python is located."
+    exit /b 1
+)
+
 setlocal EnableDelayedExpansion
 for /f "usebackq tokens=*" %%i in (`where git`) do set git="%%i"
 if %errorlevel% EQU 0 (
     if not %git% == "" (
         %pip% install virtualenv
-        virtualenv %SILA_VENV_PATH%
+        %python% -m virtualenv %SILA_VENV_PATH%
         for /f "delims=" %%i in (%pip%) do set pip="%SILA_VENV_PATH%\Scripts\%%~nxi"
 
         %git% clone -b feature/silacodegenerator-0.3 https://gitlab.com/FMeinicke/sila_python.git %~dp0\sila_python
